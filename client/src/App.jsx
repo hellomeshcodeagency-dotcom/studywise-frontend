b9e8d64bbdf24643acb2c565a72fe24349c0110f
hellomeshcodeagency-dotcom <hello.meshcodeagency@gmail.com>
2026-06-18 18:59:24 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import StudyPage      from './pages/StudyPage.jsx'
 import HistoryPage    from './pages/HistoryPage.jsx'
 import ProfilePage    from './pages/ProfilePage.jsx'
 import AdminPage      from './pages/AdminPage.jsx'
+import StudyPlanPage  from './pages/StudyPlanPage.jsx'
 import NotFoundPage   from './pages/NotFoundPage.jsx'
 
 function Spinner() {
@@ -46,6 +47,7 @@ export default function App() {
         <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
         <Route path="/study"     element={<PrivateRoute><StudyPage /></PrivateRoute>} />
         <Route path="/history"   element={<PrivateRoute><HistoryPage /></PrivateRoute>} />
+        <Route path="/plan"      element={<PrivateRoute><StudyPlanPage /></PrivateRoute>} />
         <Route path="/profile"   element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
         <Route path="/admin"     element={<AdminRoute><AdminPage /></AdminRoute>} />
         <Route path="*"          element={<NotFoundPage />} />
